bb6f48e08653b41101610c9ae4c1b4677288c45c
xujinliang <1225375045@QQ.COM>
2017-12-08 15:14:24 +0800

    增加日志

--- demo-web/src/main/java/com/liang/demo/web/controller/TestController.java
+++ demo-web/src/main/java/com/liang/demo/web/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.liang.demo.web.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/test")
 public class TestController {
 
+    public static final Logger logger = LogManager.getLogger(TestController.class);
+
     @RequestMapping(value = "/test")
     @ResponseBody
     public String test(){
+        logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!test");
         return "success";
     }
 }
