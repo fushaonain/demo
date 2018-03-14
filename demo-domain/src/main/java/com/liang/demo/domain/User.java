e8cb4cc7215ab7945f2ea71a08e4bd3711173196
xujinliang <1225375045@qq.com>
2018-03-14 14:33:47 +0800

    增加mybatis

---
+++ demo-domain/src/main/java/com/liang/demo/domain/User.java
@@ -0,0 +1,12 @@
+package com.liang.demo.domain;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer id;
+    private String userName;
+    private String password;
+}
--- demo-web/src/main/java/com/liang/demo/web/controller/TestController.java
+++ demo-web/src/main/java/com/liang/demo/web/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.liang.demo.web.controller;
 
+import com.liang.demo.domain.User;
+import com.liang.demo.service.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by xujinliang on 2017/12/7
@@ -13,12 +18,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/test")
 public class TestController {
 
+    @Resource
+    private UserService userService;
+
     public static final Logger logger = LogManager.getLogger(TestController.class);
 
     @RequestMapping(value = "/test")
     @ResponseBody
-    public String test(){
+    public List<User> test(){
         logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        return "success";
+        List<User> users = null;
+        try {
+            users = userService.getAll();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return users;
     }
 }
